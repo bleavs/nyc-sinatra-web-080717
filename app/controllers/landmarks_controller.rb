224c41292e0926bdb65e46ed7bd1f94ff50b9348
bleavs <bleavitt@colgate.edu>
2017-08-31 18:15:02 -0400

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,47 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+
+    erb :'landmarks/edit'
+  end
+
+  get '/landmarks/new' do
+
+    erb :'landmarks/new'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+
+    erb :'landmarks/show'
+  end
+
+  post '/landmarks/:id' do
+
+    @landmark = Landmark.find(params[:id])
+
+    @landmark.update(params[:landmark])
+
+
+    redirect to "/landmarks/#{@landmark.id}"
+
+  end
+
+  get '/landmarks' do
+    @landmarks = Landmark.all
+
+    erb :'landmarks/index'
+  end
+
+ post '/landmarks' do
+
+   @landmark = Landmark.create(params[:landmark])
+
+   @landmark.save
+
+  redirect to "/landmarks/#{@landmark.id}"
+ end
+
+
 end
